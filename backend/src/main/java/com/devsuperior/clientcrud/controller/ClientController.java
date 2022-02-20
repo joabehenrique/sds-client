c8eba1362c48ebc7620c5594405b69b439c142f1
joabehenrique <henriquecidoz@hotmail.com>
2022-02-20 16:29:43 -0300

    feat: adicionado clientController com operações básicas de busca, alterar, criar e deletar um novo client

--- backend/src/main/java/com/devsuperior/clientcrud/controller/ClientController.java
+++ backend/src/main/java/com/devsuperior/clientcrud/controller/ClientController.java
@@ -1,8 +1,59 @@
 package com.devsuperior.clientcrud.controller;
 
-import org.springframework.stereotype.Controller;
+import com.devsuperior.clientcrud.dto.ClientDTO;
+import com.devsuperior.clientcrud.service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-@Controller
+import java.net.URI;
 
+@RestController
+@RequestMapping(value = "/clients")
 public class ClientController {
+    @Autowired
+    private ClientService service;
+
+    @GetMapping
+    public ResponseEntity<Page<ClientDTO>> findAll(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "5") Integer linesPerPage,
+            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction
+    ){
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+
+        Page<ClientDTO> pageOfClient =  service.findAll(pageRequest);
+
+        return ResponseEntity.ok().body(pageOfClient);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> findById(@PathVariable Long id){
+        ClientDTO client = service.findById(id);
+        return ResponseEntity.ok().body(client);
+    }
+
+    @PostMapping
+    public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO clientBody){
+        ClientDTO client = service.insert(clientBody);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(clientBody.getId()).toUri();
+        return ResponseEntity.created(uri).body(client);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> update(@PathVariable Long id, @RequestBody ClientDTO clientBody){
+        ClientDTO client = service.update(id, clientBody);
+        return ResponseEntity.ok().body(client);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
