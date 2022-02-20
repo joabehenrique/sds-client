c88957b6acc57052e0b3a8cf3f009eb4de8a74f9
joabehenrique <henriquecidoz@hotmail.com>
2022-02-20 16:34:19 -0300

    feat: adicionado repository do client

---
+++ backend/src/main/java/com/devsuperior/clientcrud/repository/ClientRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.clientcrud.repository;
+
+import com.devsuperior.clientcrud.entity.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+}
